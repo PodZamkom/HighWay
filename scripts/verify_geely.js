d1b5be87548bfe053fb029f8769cd9563b88b30e
Dmitrij <Programmer.IQ@Gmail.com>
2026-02-02 12:59:19 +0300

    chore: cleanup debug scripts

--- scripts/verify_geely.js
+++
@@ -1,4 +0,0 @@
-import { importedCarsDb } from './data/cars_imported_db.js';
-const g = importedCarsDb.filter(c => c.brand.toLowerCase() === 'geely');
-console.log('Geely count:', g.length);
-if (g.length > 0) console.log('First Geely ID:', g[0].id);
